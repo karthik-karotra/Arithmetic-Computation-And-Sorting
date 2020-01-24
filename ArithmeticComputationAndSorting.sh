8aa84ed5e5bf5c83047ae2582a45098fbeda911a
karthik-karotra <karthikpatel54@gmail.com>
2020-01-24 13:01:34 +0530

    ADDED: Added dictionary and stored values of results

--- ArithmeticComputationAndSorting.sh
+++ ArithmeticComputationAndSorting.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -x
+declare -A ResultDictionary
 read -p "Enter first number: " a
 read -p "Enter second number: " b
 read -p "Enter third number: " c
@@ -7,3 +8,8 @@ result1=`echo "$a+$b*$c" | bc`
 result2=`echo "$a*$b+$c" | bc`
 result3=`echo "scale=2; $c+$a/$b" | bc`
 result4=`echo "$a%$b+$c" | bc`
+
+ResultDictionary[result1]=$result1
+ResultDictionary[result2]=$result2
+ResultDictionary[result3]=$result3
+ResultDictionary[result4]=$result4
