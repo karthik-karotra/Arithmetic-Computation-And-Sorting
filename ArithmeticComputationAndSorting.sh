1c9467048c659125ed83458c153cf3683f3a9ed2
karthik-karotra <karthikpatel54@gmail.com>
2020-01-24 11:38:14 +0530

    ADDED: Perform Fourth Computation

--- ArithmeticComputationAndSorting.sh
+++ ArithmeticComputationAndSorting.sh
@@ -6,3 +6,4 @@ read -p "Enter third number: " c
 result1=`echo "scale=2; $a+$b*$c" | bc`
 result2=`echo "scale=2; $a*$b+$c" | bc`
 result3=`echo "scale=2; $c+$a/$b" | bc`
+result4=`echo "scale=2; $a%$b+$c" | bc`
