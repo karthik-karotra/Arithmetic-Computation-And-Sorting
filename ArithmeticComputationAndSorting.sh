466f926c7760d70fe34fb8f9a9b45ed0b735826c
karthik-karotra <karthikpatel54@gmail.com>
2020-01-24 14:53:41 +0530

    ADDED: Read values from dictionary to array

--- ArithmeticComputationAndSorting.sh
+++ ArithmeticComputationAndSorting.sh
@@ -3,7 +3,7 @@ declare -A ResultDictionary
 read -p "Enter first number: " a
 read -p "Enter second number: " b
 read -p "Enter third number: " c
-
+count=0;
 result1=`echo "$a+$b*$c" | bc`
 result2=`echo "$a*$b+$c" | bc`
 result3=`echo "scale=2; $c+$a/$b" | bc`
@@ -13,3 +13,10 @@ ResultDictionary[result1]=$result1
 ResultDictionary[result2]=$result2
 ResultDictionary[result3]=$result3
 ResultDictionary[result4]=$result4
+
+while [[ $count -ne ${#ResultDictionary[@]} ]]
+do
+	array[$count]=${ResultDictionary[result"$(( count+1 ))"]}
+	count=$(( count+1 ))
+done
+echo ${array[@]}
