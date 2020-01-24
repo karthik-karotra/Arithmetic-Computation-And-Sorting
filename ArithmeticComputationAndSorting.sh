68864dbadfce23fad3287c8d66ec8bba605e911e
karthik-karotra <karthikpatel54@gmail.com>
2020-01-24 12:41:27 +0530

    REFACTORED: Corrected the formula

--- ArithmeticComputationAndSorting.sh
+++ ArithmeticComputationAndSorting.sh
@@ -3,7 +3,7 @@ read -p "Enter first number: " a
 read -p "Enter second number: " b
 read -p "Enter third number: " c
 
-result1=`echo "scale=2; $a+$b*$c" | bc`
-result2=`echo "scale=2; $a*$b+$c" | bc`
+result1=`echo "$a+$b*$c" | bc`
+result2=`echo "$a*$b+$c" | bc`
 result3=`echo "scale=2; $c+$a/$b" | bc`
-result4=`echo "scale=2; $(($a%$b))+$c" | bc`
+result4=`echo "$a%$b+$c" | bc`
